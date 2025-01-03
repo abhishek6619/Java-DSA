50021e9d93a11c0b789701c7f64301a22d1cbddc
Abhishek Kumar Singh <abhis42124@gmail.com>
2025-01-03 20:49:55 +0530

    Uppdate

--- Basics/1. Variables & Data Types/summary.java
+++ Basics/1. Variables & Data Types/summary.java
@@ -3,5 +3,6 @@ public class summary {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
