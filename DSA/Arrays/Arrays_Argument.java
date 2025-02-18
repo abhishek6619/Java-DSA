5bc82056db1cafe37839dd35af77af2f29891056
Abhishek Kumar Singh <abhis42124@gmail.com>
2025-02-18 18:52:13 +0530

    Change the Code

---
+++ DSA/Arrays/Arrays_Argument.java
@@ -0,0 +1,12 @@
+
+public class Arrays_Argument {
+
+    public static void update(int marks) {
+
+    }
+
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        int n[] = {97,98,99}; 
+    }
+}
--- DSA/Arrays/Intro_to_Arrays.java
+++ DSA/Arrays/Intro_to_Arrays.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Arrays {
+public class Intro_to_Arrays {
 
     @SuppressWarnings({"resource", "unused"})
     public static void main(String[] args) {
@@ -16,14 +16,20 @@ public class Arrays {
         marks[0] = sc.nextInt();     // phy
         marks[1] = sc.nextInt();    // chem
         marks[2] = sc.nextInt();    // math
-        marks[3] = sc.nextInt();    // math
+        marks[3] = sc.nextInt();    // cs
         System.out.println("phy : " + marks[0]);
         System.out.println("chem : " + marks[1]);
         System.out.println("math : " + marks[2]);
-        System.out.println("math : " + marks[3]);
+        System.out.println("cs : " + marks[3]);
+        marks[0] = marks[0] + 1;
+        marks[1] = marks[1] + 1;
         marks[2] = marks[2] + 1;
-        System.out.println("math : " + marks[2]);
-        int percentage = (marks[0] + marks[1] + marks[2] + marks[3]) / 3;
+        marks[3] = marks[3] + 1;
+        System.out.println("Physics : " + marks[0]);
+        System.out.println("Chemistry : " + marks[1]);
+        System.out.println("Math : " + marks[2]);
+        System.out.println("ComputerScience : " + marks[3]);
+        int percentage = (marks[0] + marks[1] + marks[2] + marks[3]) / 4;
         System.out.println("Percentage = " + percentage + "%");
     }
 }
