38e662640a113604d7c1bc1a2ed3fb6589d3d40b
Abhishek Kumar Singh <abhis42124@gmail.com>
2025-02-18 19:19:47 +0530

    Update the Codes

---
+++ DSA/Arrays/Argument.java
@@ -0,0 +1,11 @@
+
+public class Argument {
+
+    public static void update(int n) {
+    }
+
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        int n[] = {98, 89, 78};
+    }
+}
