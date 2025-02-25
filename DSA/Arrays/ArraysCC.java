e2976722a75ed37f695ec6a57870caa95adbe2bd
Abhishek Kumar Singh <abhis42124@gmail.com>
2025-02-25 23:35:56 +0530

    Update the code

--- DSA/Arrays/ArraysCC.java
+++ DSA/Arrays/ArraysCC.java
@@ -8,6 +8,8 @@ public class ArraysCC {
     }
 
     public static void main(String[] args) {
-        int marks[] = {97,98,99};
+        @SuppressWarnings("unused")
+        int marks[] = {97, 98, 99};
+
     }
 }
