1038858743b2a2cc5c5f15cc6aa2b27f61e3c9cd
Abhishek Kumar Singh <abhis42124@gmail.com>
2025-02-27 09:48:07 +0530

    Add New Code

--- DSA/Array/ArraysCC.java
+++ DSA/Array/ArraysCC.java
@@ -10,7 +10,15 @@ public class ArraysCC {
         }
     }
 
-    // @SuppressWarnings("unused")
+    public static int linearSearch(int numbers[], int key) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int marks[] = { 97, 98, 99 };
         int nonChangable = 5;
@@ -23,5 +31,17 @@ public class ArraysCC {
             System.out.println(marks[i] + "");
         }
         System.out.println();
+
+        // Linear Search
+        int numbers[] = { 2, 4, 6, 8, 10, 12, 14, 16 };
+        // String menu[] = { "dosa", "chole bhature", "samosa" };
+        int key = 20;
+
+        int index = linearSearch(numbers, key);
+        if (index == -1) {
+            System.out.println("Not Found");
+        } else {
+            System.out.println("Key is at index: " + index);
+        }
     }
 }
