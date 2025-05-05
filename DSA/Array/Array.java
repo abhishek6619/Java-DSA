1063673ee5d20f4590cce4bfe15e3f2a87d4d26a
Abhishek Kumar Singh <abhis42124@gmail.com>
2025-05-05 19:20:58 +0530

    Update the Repo

---
+++ DSA/Array/Array.java
@@ -0,0 +1,39 @@
+// Trapping Rainwater
+
+import java.util.*;
+
+public class Array {
+    public static int trappedRainwater(int height[]) {
+        int n = height.length;
+        // calculate left max boundary - array
+        int lefftMax[] = new int[n];
+        lefftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            lefftMax[i] = Math.max(height[i], lefftMax[i - 1]);
+        }
+
+        // calculate right max boundary - array
+        int rightMax[] = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+
+        int trappedWater = 0;
+        // loop
+        for (int i = 0; i < n; i++) {
+            // waterLevel = min(leftmax bound, rightmax bound)
+            int waterLevel = Math.min(lefftMax[i], rightMax[i]);
+
+            // trapped water = waterLevel - height[i]
+            trappedWater += waterLevel - height[i];
+        }
+
+        return trappedWater;
+    }
+
+    public static void main(String[] args) {
+        int height[] = { 4, 2, 0, 6, 3, 2, 5 };
+        System.out.println(trappedRainwater(height));
+    }
+}
--- DSA/DSA/Array/ArrayCC.java
+++
@@ -1,6 +0,0 @@
-public class ArrayCC {
-    public static void main(String[] args) {
-        System.out.println("Introduction to Arrays");
-        System.out.println();
-    }
-}
