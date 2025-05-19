4e18c4c7b7a9813c41bc893daea552e95e517161
Abhishek Kumar Singh <abhis42124@gmail.com>
2025-05-19 20:59:11 +0530

    Add code

--- Resources/Code Solution/Variable.java
+++ Resources/Code Solution/Variable.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Variable {
     public static void main(String[] args) {
+
+        // Average of three number
         Scanner sc = new Scanner(System.in);
         int a;
         System.out.print("Enter a: ");
@@ -14,5 +16,12 @@ public class Variable {
         c = sc.nextInt();
         int avg = (a + b + c) / 3;
         System.out.print("Average of 3 Number: " + avg);
+
+        // Square of a number
+        int num;
+        System.out.println("Enter a number: ");
+        num = sc.nextInt();
+        int square = num * num;
+        System.out.println("Square of " + num + " is: " + square);
     }
 }
